4580e8571805708a0527368d9ba569c9d7725bae
Natanael Vieira <natanaelvieira130@gmail.com>
2022-05-09 16:53:18 -0300

    M1S10 - ng-food - Exercicio 09

--- src/app/components/item-cardapio/item-cardapio.component.ts
+++ src/app/components/item-cardapio/item-cardapio.component.ts
@@ -8,14 +8,23 @@ import { IComida } from 'src/app/models/comida.model';
   styleUrls: ['./item-cardapio.component.scss']
 })
 export class ItemCardapioComponent {
-
+  quantidade: number = 0;
   @Input() item?: IComida | IBebida;
-
+  @Input() mostra: boolean = false;
   @Output() adicionaAoPedido = new EventEmitter<IComida | IBebida>();
   
   constructor() { }
 
   adicionarAoPedido() {
     this.adicionaAoPedido.emit(this.item);
+  }
+
+  incrementaQuantidade(){
+    this.quantidade++;
+  }
+
+  decrementaQuantidade(){
+    this.quantidade--;
+    this.quantidade = this.quantidade < 0 ? 0 : this.quantidade;
   }
 }
